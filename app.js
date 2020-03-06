def159fd1844b775a38673f5359d341c6ad932af
Yamoto Ryousuke <yahhoi3379@gmail.com>
2020-03-06 14:05:22 +0900

    build完了

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 
+
 mongoose.connect('mongodb+srv://yamoto:kiguruma@cluster0-eshrb.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
 
 const todoSchema = mongoose.Schema({
@@ -33,12 +34,15 @@ app.use(cors())
 app.use(bodyParser());
 
 
+app.use(express.static('build'))
+
 
 app.get('/todos', (req, res) => {
     //この/todosは3001/todos?
     //3001/todosにgetしたらTodoをfindして
     //成功したらmongodbのtodosをjson化して3001に飛ばす？
     Todo.find({})
+    //このtodosは何？mongoDBのtodos?
         .then(todos => {
             res.json(todos.map(todo => todo.toJSON()))
         })
@@ -74,9 +78,13 @@ app.put('/todos/:id', function (req, res) {
 
     const todo = new Todo({ _id: req.params.id})
 
+    console.log('content',req.body.content)
+
+    console.log('id',req.params.id)
+
     todo.updateOne({ content: req.body.content})
     // .then( todo => {
-    //     res.json(todo.toJSON())
+    //     console.log(todo.data)
     // })
 })
 
@@ -90,13 +98,16 @@ app.delete('/todos/:id', function (req, res) {
     const todo = new Todo({ _id: req.params.id})
 
     todo.deleteOne()
+    .then(todo => {
+        res.status(204).end()
+    })
     //今mongo上のみ消えてる
     
 
-    Todo.find({})
-        .then(todos => {
-            res.json(todos.map(todo => todo.toJSON()))
-        })
+    // Todo.find({})
+    //     .then(todos => {
+    //         res.json(todos.map(todo => todo.toJSON()))
+    //     })
 
     // then(todos => {
     //     res.json(todos.map(todo => todo.toJSON()))
---
+++ build/precache-manifest.eace532749e850af02cb69a34172ef7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f3642b992f3eb1e192bc454f381654",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34cc9c5d0fc27e5ae0c2",
+    "url": "/static/css/main.1af85b8a.chunk.css"
+  },
+  {
+    "revision": "0f7cc904cc1d961d2c2b",
+    "url": "/static/js/2.52fc3c8f.chunk.js"
+  },
+  {
+    "revision": "f6ab5397c4eecf182dd2e7f66a22dd4b",
+    "url": "/static/js/2.52fc3c8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34cc9c5d0fc27e5ae0c2",
+    "url": "/static/js/main.31323d1d.chunk.js"
+  },
+  {
+    "revision": "67e3cf4e5818f2b0d075",
+    "url": "/static/js/runtime-main.cd790e21.js"
+  }
+]);
