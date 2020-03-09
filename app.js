a3ff9571f9fd42a137dc853c58aa9bd5ac46eddd
Yamoto Ryousuke <yahhoi3379@gmail.com>
2020-03-09 17:13:53 +0900

    updateOne完成

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ mongoose.connect('mongodb+srv://yamoto:kiguruma@cluster0-eshrb.mongodb.net/test?
 
 const todoSchema = mongoose.Schema({
     //ここでcontent定義？それからどうやってApp.jsに持ってきた？
+    //_idはmongoooseの仕様上必ず着く　_vも
     content: String
 })
 
 todoSchema.set('toJSON',{
     transform: (doc, todo) => {
         todo.id = todo._id.toString()
-        //todoschemaの中のtodoの_idを文字列化して
+        //todoschemaの中のtodoの_idを文字列化してidに変換するtoJSONメソッドを作る
         delete todo._id
         delete todo.__v
     }
@@ -38,11 +39,12 @@ app.use(express.static('build'))
 
 
 app.get('/todos', (req, res) => {
-    //この/todosは3001/todos?
-    //3001/todosにgetしたらTodoをfindして
-    //成功したらmongodbのtodosをjson化して3001に飛ばす？
+    //この/todosは3001/todos
+    //3001/todosにget送信来たらmongoDBのTodoモデルをfindする
+    //findはmongooseのメソッド
+    //成功したらmongodbのtodosをjson化してreactに飛ばす　これはあくまでexpressの処理
     Todo.find({})
-    //このtodosは何？mongoDBのtodos?
+    //このtodosは仮引数　中身はTodo.find({})した結果が入ってる
         .then(todos => {
             res.json(todos.map(todo => todo.toJSON()))
         })
@@ -62,36 +64,36 @@ const generateId = () => {
 app.post('/todos',(req, res) => {
 
     const todo = new Todo({ content: req.body.content})
-    //３００１に追加されたnewTodoのcontentを取り出してる？
-    //req.body.contentどこから来た？
-    //サーバー＝3001/todos?から送られてきた
-    //どうゆう順番？post ブラウザ(React)=>サーバー（３００１）＝＞mongo?
+    //３００１にpost送信が来たらnew演算子でtodo作成
+    //req.body.contentはnewTodoに入ってる
+    //どうゆう順番？post送信来る＝＞todo作る＝＞tosoをmongoDB上に保存
     todo.save()
-    //todo.saveでmongodbに追加された
-    //それでまた3001にレスポンス返してる？二回通ってる？
+    //下のtodoには上の中身が入っている
+    //todoをtoJSONして送っている
     .then( todo => {
         res.json(todo.toJSON())
     })
 })
 
 app.put('/todos/:id', function (req, res) {
+    //PUT送信来たらTODO作るIDがreq.params.idの
 
     const todo = new Todo({ _id: req.params.id})
 
-    console.log('content',req.body.content)
+    // console.log('content',req.body.content)
 
-    console.log('id',req.params.id)
+    // console.log('id',req.params.id)
 
     todo.updateOne({ content: req.body.content})
-    // .then( todo => {
-    //     console.log(todo.data)
-    // })
+    .then( todo => {
+        res.json(todo)
+    })
 })
 
 
 app.delete('/todos/:id', function (req, res) {
 
-    // ブラウザから３００１にdelete送信。
+    // ブラウザから３００１にdelete送信。delete送信来たらmongo上のtodo消す
 
     //mongo上だと_idなので形を直す
 
@@ -99,30 +101,13 @@ app.delete('/todos/:id', function (req, res) {
 
     todo.deleteOne()
     .then(todo => {
+    //todoは仮引数
         res.status(204).end()
     })
     //今mongo上のみ消えてる
-    
-
-    // Todo.find({})
-    //     .then(todos => {
-    //         res.json(todos.map(todo => todo.toJSON()))
-    //     })
-
-    // then(todos => {
-    //     res.json(todos.map(todo => todo.toJSON()))
-    // })
-
-
-        // todos =  todos.filter(todo => todo.id !=
-        //         req.params.id
-        //     )
-        //     console.log(todos.length,'delete後')
-
-        //     res.status(204).end()
-
 })
 
 app.listen(3001, () => {
+    //expressは3001番で受け付けている
     console.log('Server Started')
 })
